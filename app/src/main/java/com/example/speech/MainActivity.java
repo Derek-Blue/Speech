669d1061891fa7475a261fd7572daae960e89cc1
a984983 <a984983@gmail.com>
2019-11-04 10:27:08 +0800

    11/4課程 Speech 1.0.02

--- app/src/main/java/com/example/speech/MainActivity.java
+++ app/src/main/java/com/example/speech/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.speech;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,6 +44,31 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                 getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent,REQ_CODE_SPEECH_INPUT);
+        }catch (ActivityNotFoundException a){
+            Toast.makeText(getApplicationContext(),
+                    "對不起，您的設備沒有支援",
+                    Toast.LENGTH_SHORT).show();
+        }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && data != null) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 }
