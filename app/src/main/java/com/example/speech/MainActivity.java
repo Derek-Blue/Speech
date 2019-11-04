79d7028c9f32b6e6c0aa3615c7120567f9b88458
a984983 <a984983@gmail.com>
2019-11-04 09:41:55 +0800

    11/4課程 Speech 1.0.01

--- app/src/main/java/com/example/speech/MainActivity.java
+++ app/src/main/java/com/example/speech/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.speech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +29,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                promptSpeechInput();
+
             }
         });
+    }
+    public void  promptSpeechInput(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+
     }
 }
